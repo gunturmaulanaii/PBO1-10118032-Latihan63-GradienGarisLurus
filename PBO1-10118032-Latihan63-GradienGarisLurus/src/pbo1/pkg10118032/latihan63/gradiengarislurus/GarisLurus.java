67ef5a96ba151263d71cf3a82043229fd04a7954
Guntur Maulana Ibrahim <55784684+gunturmaulanaii@users.noreply.github.com>
2019-10-24 20:11:03 +0700

    Add files via upload

---
+++ PBO1-10118032-Latihan63-GradienGarisLurus/src/pbo1/pkg10118032/latihan63/gradiengarislurus/GarisLurus.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118032.latihan63.gradiengarislurus;
+
+/**
+ *
+ * @author Guntur Maulana I
+ */
+public interface GarisLurus {
+    
+    public int hitungGradien();
+    
+}
